622f0029298e6e5b8256dc3448baad803687aba8
rakshith_s <rakshithsantosh@gmail.com>
2024-07-09 14:34:10 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "LUNA"),
+    React.createElement("h2", {}, "DOG"),
+    React.createElement("h2", {}, "SHIBA"),
+  ]);
+};
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
