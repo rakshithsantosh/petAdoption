5aa574d433c49e2b6777539f9ba0df09fd762e04
rakshith_s <rakshithsantosh@gmail.com>
2024-07-10 12:51:11 +0530

    mapping through data with hooks

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,12 +1,38 @@
 import {useState} from 'react';
+const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
 const SearchParams = () =>{
     const [location,setLocation] = useState("");
+    const [animals,setAnimals] = useState("");
+    const [breed,setBreed] = useState("");
+    const breeds=[];
     return(
         <div className='search-params'>
             <form>
                 <label htmlFor="location">
                     Location 
                     <input onChange={(e)=>{setLocation(e.target.value)}}type="text" id="location" placeholder='location' value={location} />
+                </label>
+                <label htmlFor="animals">
+                    Animal
+                    <select value={animals}id="animals"
+                    onChange={e=>{setAnimals(e.target.value);
+                        setBreed("");
+                    }}>
+                        <option/>
+                        {ANIMALS.map((animal)=>{
+                            return <option key={animal}>{animal}</option>
+                        })}
+                    </select>
+                </label>
+                <label htmlFor="breed">
+                    Breed
+                    <select value={animals} id="breed" disabled={breeds.length ===0}
+                    onChange={e=>{setBreed(e.target.value)}}>
+                        <option/>
+                        {breeds.map((breed)=>{
+                            return <option key={breed}>{breed}</option>
+                        })}
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
