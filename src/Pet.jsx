9dcf1592e6f262c274475759654d55251ba68ad2
rakshith_s <rakshithsantosh@gmail.com>
2024-07-12 15:24:00 +0530

    modifying the pet component

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,17 +1,22 @@
 const Pet = (props) => {
-    /*return React.createElement("div", {}, [
-      React.createElement("h1", {}, "LUNA"),
-      React.createElement("h2", {}, "DOG"),
-      React.createElement("h2", {}, "SHIBA"),
-    ]);*/
-
-    return(
-        <div>
-            <h1>{props.name}</h1>
-            <h1>{props.animal}</h1>
-            <h1>{props.breed}</h1>
+    const { name, animal, breed, images, location, id } = props;
+  
+    let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+    if (images.length) {
+      hero = images[0];
+    }
+  
+    return (
+      <a href={`/details/${id}`} className="pet">
+        <div className="image-container">
+          <img src={hero} alt={name} />
         </div>
-    )
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+        </div>
+      </a>
+    );
   };
-
+  
   export default Pet;
